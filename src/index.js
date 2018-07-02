221aeeaf4365430e52cb85b205b5f63675ff5d9b
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-02 17:25:55 +0100

    Convert App to a stateful, class-based component
    With a stateless functional component for the root it would be
    difficult to hold and update the state of the game.

--- src/index.js
+++ src/index.js
@@ -5,16 +5,24 @@ import GameOfLife from "game-of-life-logic";
 
 import "./styles.css";
 
-function App() {
-  const game = new GameOfLife(5, 5);
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>
-        Created a {game.width} x {game.height} game.
-      </h2>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { game: new GameOfLife(5, 5) };
+  }
+
+  render() {
+    const game = this.state.game;
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>
+          Created a {game.width} x {game.height} game.
+        </h2>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");

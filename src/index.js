bd7f780af9ac7de3e22bc7d75fdfb12124ba1feb
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-02 17:37:21 +0100

    Handle clicks on individual cells
    It is a bit of an anti-pattern to have to forceUpdate in
    React, but the library we are using expects the game to be
    modified, rather than creating a new object we can setState
    with.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ class App extends React.Component {
     this.state = { game: new GameOfLife(5, 5) };
   }
 
+  clicked = (row, col) => {
+    const oldValue = this.state.game.matrix[row][col];
+    const newValue = oldValue === 0 ? 1 : 0;
+    this.state.game.copyMatrixAt(row, col, [[newValue]]);
+    this.forceUpdate();
+  };
+
   render() {
     const game = this.state.game;
     return (
@@ -21,11 +28,15 @@ class App extends React.Component {
           Created a {game.width} x {game.height} game.
         </h2>
         <div className="matrix">
-          {game.matrix.map((row, rowIndex) => {
-            return (<div className="row" key={rowIndex}>
-              {row.map((cell, colIndex) => <div className="cell" key={colIndex}>{cell}</div>)}
-            </div>)
-          })}
+          {game.matrix.map((row, rowIndex) => (
+              <div className="row" key={rowIndex}>
+                {row.map((cell, colIndex) => (
+                    <div className="cell" key={colIndex} onClick={() => this.clicked(rowIndex, colIndex)}>
+                      {cell}
+                    </div>
+                ))}
+              </div>
+          ))}
         </div>
       </div>
     );
